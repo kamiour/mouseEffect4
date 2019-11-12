509bdc2fab43af226e5969cf3a59f339047111ff
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2019-11-12 19:15:22 +0200

    added counter

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-let number = 100;
+let number = 200;
 let diameter = 4;
 let initialLeft = [];
 let initialTop = [];
 let points = [];
-let maxWidth = 250;
-
+let counter = 0;
 for (let i = 0; i < number; i++) {
   $("#one").append( '<div id="move-'+i+'" class="move"></div>');
 }
@@ -23,6 +22,7 @@ for (let i = 0; i < elementsArr.length; i++) {
     
 $('img').click(function() {
     points = [];
+    counter = 0;
     if(!this.canvas) {
         this.canvas = $('<canvas />')[0];
         this.canvas.width = this.width;
@@ -33,21 +33,31 @@ $('img').click(function() {
     let startLeft = $('img')[0].offsetLeft;
     let startTop = $('img')[0].offsetTop;
 
-    for (let i = 0; i<this.width; i+=4) {
-      for (let j = 0; j<this.height; j+=4) {
+    for (let i = 0; i<this.width; i+=diameter) {
+        for (let j = 0; j<this.height; j+=diameter) {
+            var pixelData = this.canvas.getContext('2d').getImageData(i, j, 1, 1).data;
+            if (pixelData[0] !== 255 && pixelData[1] == !255 && pixelData[2] == !255) {
+                counter++;
+            }
+        }
+    }
+    console.log(counter);
+
+    for (let i = 0; i<this.width; i+=diameter) {
+      for (let j = 0; j<this.height; j+=diameter) {
         var pixelData = this.canvas.getContext('2d').getImageData(i, j, 1, 1).data;
         if (pixelData[0] !== 255 && pixelData[1] == !255 && pixelData[2] == !255) {
           points[points.length] = {
             x: i,
             y: j,
           };
-          console.log('black');
-        } else console.log('white');
+        }
       }
     }
+    console.log(points.length);
 
     for (let p = 0; p < points.length; p++) {
-      elementsArr[p].style.left = points[p].x + startLeft;
-      elementsArr[p].style.top = points[p].y + startTop;
+      elementsArr[p].style.left = points[p].x + startLeft - diameter/2;
+      elementsArr[p].style.top = points[p].y + startTop - diameter/2;
     }
 });
